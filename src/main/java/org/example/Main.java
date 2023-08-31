6ee79694478e41c407e3937d6c942f92adbcd293
JoergWolff <neuerfisch@wolffclan.de>
2023-08-31 11:06:45 +0200

    change output to english

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         MediaController mediaController = new MediaController();
-
+        System.out.println("Music Player\n--------");
         MusicPlayer musicPlayer = new MusicPlayer("Invisible_Man.mp3");
         mediaController.playMedia(musicPlayer);
+        System.out.println("Video Player\n--------");
         VideoPlayer videoPlayer = new VideoPlayer("Herr_der_Ringe.mp4");
         mediaController.playMedia(videoPlayer);
+
     }
 }
